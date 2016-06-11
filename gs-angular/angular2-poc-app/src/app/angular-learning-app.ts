9ea675552186eefa1a89d8aaa39ec932459f094c
Tirthal Patel <patel.tirthal@gmail.com>
2016-06-11 14:08:38 +0530

    Created initial project structure of Angular 2 app for building POC

---
+++ gs-angular/angular2-poc-app/src/app/angular-learning-app.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+@Component({
+	  selector: 'angular-learning-app',
+	  providers: [],
+	  pipes: [],
+	  directives: [],
+	  templateUrl: 'app/angular-learning-app.html',
+})
+export class AngularLearningApp {
+
+  constructor() {}
+
+}
